5e45d7cd8c2310bd8e0643d411c7e61bd36214ca
Frinze Erin Lapuz <frinzekt@gmail.com>
2019-11-25 16:50:52 +0800

    ajax load

--- CTRload.js
+++ CTRload.js
@@ -1,4 +1,44 @@
-function pageload(projectId) {
-	console.log("FETCHING DATA");
-	//console.log(getProjectSubjobs(projectId));
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(
+		m,
+		key,
+		value
+	) {
+		vars[key] = value;
+	});
+	return vars;
 }
+
+function pageload() {
+	projectId = getUrlVars()["projectId"];
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: "http://localhost:3000/query/fetch.php",
+			method: "POST",
+			data: {
+				projectId: projectId
+			},
+			success: data => {
+				data = JSON.parse(data);
+				console.log(data);
+				resolve(data);
+			}
+		});
+	});
+}
+
+unpackInvoicedAmount = data => {
+	[id, ...invoicedAmount] = data.invoicedAmountGroupByJob;
+	return invoicedAmount;
+};
+
+unpackReconciledAmount = data => {
+	[id, ...reconciledAmount] = data.reconciledAmountGroupByJob;
+	return reconciledAmount;
+};
+
+unpackReconciledAmount = data => {
+	[id, ...reconciledAmount] = data.reconciledAmountGroupByJob;
+	return reconciledAmount;
+};
